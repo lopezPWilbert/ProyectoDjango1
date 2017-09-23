6fe46dd010792123469edbc6c4b5801197167c1f
Alex López <Alex Lpez>
2017-09-23 11:29:02 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoDjango1/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alumn\documents\visual studio 2017\Projects\ProyectoDjango1\ProyectoDjango1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
